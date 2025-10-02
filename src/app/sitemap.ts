bab0a90c90124e7f36069d5f7312fffcfc1086e3
Richard <richard.star268@gmail.com>
2025-10-02 13:16:45 -0400

    updated sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,31 +3,31 @@ import { MetadataRoute } from 'next'
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://www.tjatc.org',
+      url: 'https://tjatc.org',
       lastModified: new Date(),
       changeFrequency: 'daily',
       priority: 1,
     },
     {
-      url: 'https://www.tjatc.org/atconnect',
+      url: 'https://tjatc.org/atconnect',
+      lastModified: new Date(),
+      changeFrequency: 'daily',
+      priority: 1,
+    },
+    {
+      url: 'https://tjatc.org/atconnect25',
       lastModified: new Date(),
       changeFrequency: 'daily',
       priority: 0.8,
     },
     {
-      url: 'https://www.tjatc.org/atconnect25',
-      lastModified: new Date(),
-      changeFrequency: 'daily',
-      priority: 0.8,
-    },
-    {
-      url: 'https://www.tjatc.org/about',
+      url: 'https://tjatc.org/about',
       lastModified: new Date(),
       changeFrequency: 'daily',
       priority: 0.5,
     },
     {
-      url: 'https://www.tjatc.org/games',
+      url: 'https://tjatc.org/games',
       lastModified: new Date(),
       changeFrequency: 'daily',
       priority: 0.5,
