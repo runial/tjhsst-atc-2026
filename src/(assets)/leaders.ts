3c67a7ca8151087c584e6ff49b7c0ac10a37158e
Uzair Nasir <108600396+uzair-nasir@users.noreply.github.com>
2026-05-23 22:04:03 -0400

    Update leaders.ts

--- src/(assets)/leaders.ts
+++ src/(assets)/leaders.ts
@@ -4,7 +4,7 @@ export const leaders: Leader[] = [
   {
     name: "Tanmayi Mendu",
     role: "President",
-    image: "/leadership/2026_2027/tanmayi_mendu.webp",
+    image: "/leadership/2026_2027/placeholder.webp",
     imageAlt: "Tanmayi Mendu"
   },
   {
@@ -16,13 +16,13 @@ export const leaders: Leader[] = [
   {
     name: "Nabi Zekarias",
     role: "Vice President",
-    image: "/leadership/2026_2027/nabi_zekarias.webp",
+    image: "/leadership/2026_2027/placeholder.webp",
     imageAlt: "Nabi Zekarias"
   },
   {
     name: "Uzair Nasir",
     role: "Webmaster",
-    image: "/leadership/2026_2027/uzair_nasir.webp",
+    image: "/leadership/2026_2027/placeholder.webp",
     imageAlt: "Uzair Nasir"
   },
   {
